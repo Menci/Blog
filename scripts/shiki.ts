5f2c2a7897b30da8d943e7a8c42b5b45e1a8d223
Menci <mencici@msn.com>
2024-11-28 18:20:26 +0800

    Upgrade dependencies

--- scripts/shiki.ts
+++ scripts/shiki.ts
@@ -1,22 +1,24 @@
 import fs from "node:fs";
 import path from "node:path";
 
-import { getHighlighterCore } from "shiki/core";
 import loadWasm from "shiki/wasm";
 import githubLight from "shiki/themes/github-light.mjs";
-import { bundledLanguages } from "shiki";
 
 const CUSTOM_SYNTAXES_PATH = path.resolve(__dirname, "..", "syntaxes");
 const customLanguages = fs.readdirSync(CUSTOM_SYNTAXES_PATH).filter(filename => filename.endsWith(".tmLanguage.json"));
 
-const highlighter = getHighlighterCore({
-  langs: [
-    ...Object.values(bundledLanguages),
-    ...customLanguages.map(filename => JSON.parse(fs.readFileSync(path.join(CUSTOM_SYNTAXES_PATH, filename), "utf-8")))
-  ],
-  themes: [githubLight],
-  loadWasm
-});
+const highlighter = Promise.all([import("shiki/core"), import("shiki")]).then(([shikiCore, shiki]) =>
+  shikiCore.getSingletonHighlighterCore({
+    langs: [
+      ...Object.values(shiki.bundledLanguages),
+      ...customLanguages.map(filename =>
+        JSON.parse(fs.readFileSync(path.join(CUSTOM_SYNTAXES_PATH, filename), "utf-8"))
+      )
+    ],
+    themes: [githubLight],
+    loadWasm
+  })
+);
 
 export async function highlight(code: string, language: string) {
   return (await highlighter).codeToHtml(code, {
